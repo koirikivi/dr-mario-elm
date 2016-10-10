6f70ae4ef16ae64d5c56fde4017eb215c5a68102
Rainer Koirikivi <rainer@koirikivi.fi>
2016-10-10 16:21:50 +0300

    Initial combo function

--- Board.elm
+++ Board.elm
@@ -1,5 +1,6 @@
 module Board exposing (..)
 import Array exposing (Array)
+import Dict exposing (Dict)
 import Maybe exposing (andThen, withDefault)
 
 
@@ -51,6 +52,9 @@ testBoard =
     |> set (4, 6) (Pill Red None False)
     |> set (0, 0) (Pill Yellow None False)
     |> set (6, 3) (Virus Yellow)
+    |> set (6, 5) (Pill Yellow None False)
+    |> set (6, 6) (Pill Yellow None False)
+    |> set (6, 7) (Pill Yellow None False)
 
 set : Position -> Block -> Board -> Board
 set (x, y) block board =
@@ -70,6 +74,11 @@ get (x, y) board =
   (Array.get y board `andThen` Array.get x)
     |> withDefault Empty
 
+blocks : Board -> List Block
+blocks board =
+  positionedBlocks board
+    |> List.map (\((x, y), block) -> block)
+
 positionedBlocks : Board -> List (Position, Block)
 positionedBlocks board =
   Array.toIndexedList board
@@ -78,11 +87,19 @@ positionedBlocks board =
         |> List.map (\(x, block) -> ((x, y), block)))
     |> List.concat
 
--- create a new board by iterating blocks bottom-up
+-- create a new board by iterating indexed blocks bottom-up
 foldb : ((Position, Block) -> Board -> Board) -> Board -> Board
 foldb callback board =
   positionedBlocks board
     |> List.foldl callback board
+
+isFalling : Block -> Bool
+isFalling block =
+  case block of
+    Pill _ _ falling ->
+      falling
+    _ ->
+      False
 
 -- GAME LOGIC
 
@@ -90,12 +107,81 @@ update : Board -> Board
 update board =
   board
     |> applyGravity
+    |> (
+      if List.any isFalling (blocks board) then
+        identity
+      else
+        applyCombos
+      )
 
 applyGravity : Board -> Board
 applyGravity board =
   board
     |> foldb markFalling
     |> foldb moveFalling
+
+applyCombos : Board -> Board
+applyCombos board =
+  let
+    colorPositions =
+      positionedBlocks board
+        |> List.filterMap (\(pos, block) ->
+          case block of
+            -- only blocks that are not falling are counted
+            Pill color _ False ->
+              Just (pos, color)
+            Virus color ->
+              Just (pos, color)
+            _ ->
+              Nothing
+          )
+    combos = detectCombos colorPositions
+  in
+    combos
+      |> List.concat
+      |> List.foldl (\position board -> set position Empty board) board
+
+-- this is not yet final..
+-- - combos on same row or column may be detected twice
+-- - combos is a misnomer, perhaps
+detectCombos : List (Position, Color) -> List (List Position)
+detectCombos colorPositions =
+  case colorPositions of
+    [] ->
+      []
+    (pos, color)::remaining ->
+      let
+        colorMap = Dict.fromList remaining
+        up = detectNearbyCombo (0, 1) pos color colorMap
+        right = detectNearbyCombo (1, 0) pos color colorMap
+        -- we proceed from up-right from the bottom-left corner,
+        -- so some of this can be skipped
+        --down = detectNearbyCombo (0, -1) pos color colorMap
+        --left = detectNearbyCombo (-1, 0) pos color colorMap
+        --horizontal = List.concat [ up, [pos], down ]
+        --vertical = List.concat [ left, [pos], right ]
+        vertical = pos :: up
+        horizontal = pos :: right
+      in
+        ( if List.length vertical >= 4 then [ vertical ] else []
+        ++ if List.length horizontal >= 4 then [ horizontal ] else []
+        ++ detectCombos remaining
+        )
+
+
+detectNearbyCombo : Position -> Position -> Color -> Dict Position Color -> List Position
+detectNearbyCombo (dirX, dirY) (posX, posY) color colorMap =
+  let
+    (newX, newY) = (dirX + posX, dirY + posY)
+  in
+    case Dict.get (newX, newY) colorMap of
+      Just newColor ->
+        if newColor == color then
+            (newX, newY) :: detectNearbyCombo (dirX, dirY) (newX, newY) color colorMap
+        else
+          []
+      Nothing ->
+        []
 
 markFalling : (Position, Block) -> Board -> Board
 markFalling ((x, y), block) board =
